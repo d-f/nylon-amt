ac78cfb406b3bb2b31c66c7660ff65cd76a82e31
Daniel Franklin <118086192+d-f@users.noreply.github.com>
2024-09-12 21:56:38 -0600

    Update dataset_creation.py

--- dataset_creation.py
+++ dataset_creation.py
@@ -1,62 +1,67 @@
-from mido import MidiFile
+import pretty_midi
 import matplotlib.pyplot as plt
-# from transformers import T5ForConditionalGeneration, T5Tokenizer, T5Config
 import torch
-from scipy.io import wavfile
-import numpy as np
 import torchaudio
 import librosa
-import numpy as np
 import librosa
 import matplotlib.pyplot as plt
-from scipy.ndimage import median_filter
-from scipy.spatial.distance import cdist
+import argparse
+from pathlib import Path
+import numpy as np
 
 
-def load_wav(file_path):
-    audio, sr = librosa.load(file_path, mono=True, sr=16000)
-    return audio, sr
+def parse_cla() -> None:
+    """
+    parses command line arguments
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-midi_dir", type=Path)
+    parser.add_argument("-audio_dir", type=Path)
+    return parser.parse_args()
 
 
-def plot_spectrogram(spectrogram):
-#     mel_bins = spectrogram.mel_scale.fb.size(0)
-#     mel_freqs = torchaudio.functional.melscale_frequencies(
-#     n_mels=mel_bins,
-#     f_min=0.0,
-#     f_max=16000 / 2.0,
-# )
+def load_wav(file_path: str, sample_rate: int) -> tuple[np.array, int]:
+    """
+    loads wav file at a given sample rate and returns a numpy array
+    of the loaded audio
+
+    file_path   -- path to the wav file
+    sample_rate -- sample rate at which the file should be loaded
+    """
+    audio = librosa.load(file_path, mono=True, sr=sample_rate)
+    return audio
+
+
+def plot_spectrogram(spectrogram: np.array) -> None:
+    """
+    plots spectrogram data
+
+    spectrogram -- np.array of spectrogram values
+    """
     plt.title("Mel Spectrogram")
     plt.ylabel("Mel bins")
     plt.xlabel("Time")
     plt.imshow(spectrogram, origin="lower", aspect="auto")
-    # plt.figure(figsize=(10, 4))
-    # plt.imshow(spectrogram, origin='lower', aspect='auto', cmap='viridis')
-    # plt.colorbar(format='%+2.0f dB')
-    # plt.title('Mel spectrogram (Hz scale)')
-    # plt.xlabel('Time')
-    # plt.ylabel('Frequency (Hz)')
-    # plt.yticks(np.linspace(0, mel_bins - 1, num=10), np.round(mel_freqs[np.linspace(0, mel_bins - 1, num=10).astype(int)], 1))
-    # plt.tight_layout()
     plt.show()
 
 
-def audio_to_mel(audio, sr):
-    trans = torchaudio.transforms.MelSpectrogram(sample_rate=sr, n_mels=40)
-    mel_spectrogram = trans(torch.tensor(audio))
-    
+def audio_to_mel(audio: np.array, sr: int, n_mels: int) -> torch.tensor:
+    """
+    converts audio to a mel spectrogram
+
+    audio  -- np.array of loaded audio values
+    sr     -- int that determines the sample rate for mel spectrogram conversion
+    n_mels -- number of mel spectrogram bins
+    """
+    trans = torchaudio.transforms.MelSpectrogram(sample_rate=sr, n_mels=n_mels)
+    mel_spectrogram = trans(torch.tensor(audio))    
     return mel_spectrogram
 
 
-def prepare_input(mel_spectrogram, tokenizer):
-
-    input_str = ' '.join(map(str, mel_spectrogram.flatten().tolist()))
-    
-
-    inputs = tokenizer(input_str, return_tensors="pt", padding=True, truncation=True)
-    return inputs
-
-
-def plot_audio(audio):
+def plot_audio(audio: np.array) -> None:
+    """
+    plot the audio waveform
+    """
     plt.title("Waveform")
     plt.xlabel("Time")
     plt.ylabel("Normalized amplitude")
@@ -64,172 +69,95 @@ def plot_audio(audio):
     plt.show()
 
 
-def convert_to_DB(mel_spectrogram):
-    mel_spectrogram_db = torchaudio.functional.amplitude_to_DB(x=mel_spectrogram, multiplier=10, amin=1e-10, db_multiplier=0.0, top_db=80.0)
+def convert_to_DB(
+        mel_spectrogram: np.array, 
+        multiplier: int, 
+        amin: float, 
+        db_multiplier: float, 
+        top_db: float
+        ) -> np.array:
+    """
+    converts amplitude values to decibles
+
+    mel_spectrogram -- np.array of spectrogram values
+    multiplier      -- 10 used for power, 20 for amplitude
+    amin            -- lower threshold value
+    db_multiplier   -- scaling factor
+    top_db          -- minimum negative cut-off in db
+    """
+    mel_spectrogram_db = torchaudio.functional.amplitude_to_DB(
+        x=mel_spectrogram, 
+        multiplier=multiplier, 
+        amin=amin, 
+        db_multiplier=db_multiplier, 
+        top_db=top_db
+        )
     return mel_spectrogram_db
 
 
-def norm_sg(spectrogram):
+def norm_sg(spectrogram: np.array) -> np.array:
+    """
+    normalizes spectrogram values
+    """
     s_db = (spectrogram - spectrogram.min()) / (spectrogram.max() - spectrogram.min())
     return s_db
 
 
 def norm_midi_pitch(midi_pitches):
+    """
+    normalizes midi pitch values
+    """
     return midi_pitches / 127
 
 
-def align(audio, sr, midi):
-    midi_onsets = []
-    midi_pitches = []
-    current_time = 0
+def create_tensor(debug_vis: bool, audio_path: str, midi_path: str) -> torch.tensor:
+    """
+    creates a tensor from an audio and midi file
 
-    for msg in midi:
-        current_time += msg.time
-        if not msg.is_meta and msg.type == "note_on" and msg.velocity > 0:
-            midi_onsets.append(current_time)
-            midi_pitches.append(msg.note)
+    debug_vis  -- boolean that determines if visualizations 
+                  are done for debugging purposes
+    audio_path -- path to the .wav file
+    midi_path  -- path to the .midi file
+    """
+    audio = load_wav(audio_path, sr=16000)
 
-    midi_onsets = np.array(midi_onsets)
-    midi_pitches = np.array(midi_pitches)
-
-    # Audio Onset Detection
-    onset_env = librosa.onset.onset_strength(y=audio, sr=sr, max_size=1, lag=1)
-
-    # Normalize the onset envelope
-    onset_env = librosa.util.normalize(onset_env)
-
-    # Apply a median filter to smooth the onset envelope
-    onset_env = median_filter(onset_env, size=3)
-
-    # Adjust the parameters for peak picking to capture more onsets initially
-    pre_max = 3
-    post_max = 3
-    pre_avg = 3
-    post_avg = 3
-    delta = 0.01  # Lower threshold to capture more peaks
-    wait = 1
-
-    # Use peak picking directly on the onset envelope
-    peaks = librosa.util.peak_pick(onset_env, pre_max=pre_max, post_max=post_max, pre_avg=pre_avg, post_avg=post_avg, delta=delta, wait=wait)
-    audio_onsets_initial = librosa.frames_to_time(peaks, sr=sr)
-
-    # Use DTW to align MIDI onsets to audio onsets
-    distance, path = dtw(midi_onsets, audio_onsets_initial, dist=cdist)
-
-    # Extract the aligned audio onsets
-    aligned_audio_onsets = [audio_onsets_initial[j] for (i, j) in path]
-    # midi_onsets = []
-    # midi_pitches = []
-
-    # current_time = 0
-
-    # for msg in midi:
-    #     current_time += msg.time
-    #     if not msg.is_meta and msg.type == "note_on" and msg.velocity > 0:
-    #         midi_onsets.append(current_time)
-    #         midi_pitches.append(msg.note)
-
-    # midi_onsets = np.array(midi_onsets)
-    # midi_pitches = np.array(midi_pitches)
-
-
-
-
-    # onset_env = librosa.onset.onset_strength(y=audio, sr=sr, max_size=1, lag=1)
-
-    # # Normalize and smooth the onset envelope
-    # onset_env = librosa.util.normalize(onset_env)
-    # # onset_env = librosa.effects.harmonic(onset_env)
-
-    # # Adjust the threshold and sensitivity
-    # # onset_threshold = np.mean(onset_env) + np.std(onset_env)  # Lowered threshold
-    # # audio_onsets = librosa.onset.onset_detect(onset_envelope=onset_env, sr=sr, units='time', backtrack=True, delta=onset_threshold, wait=1)
-
-    # # Use peak picking directly on the onset envelope
-    # peaks = librosa.util.peak_pick(onset_env, pre_max=1, post_max=1, pre_avg=1, post_avg=1, delta=0.0005, wait=1)
-    # audio_onsets = librosa.frames_to_time(peaks, sr=sr)
-
-    # pitches, magnitudes = librosa.core.piptrack(y=audio, sr=sr)
-    # onset_frames = librosa.time_to_frames(audio_onsets, sr=sr)
-    # # audio_pitches = []
-
-    # onset_frames = onset_frames[onset_frames < pitches.shape[1]]
-
-    # extracted_pitches = []
-    # for onset in onset_frames:
-    #     # frame = librosa.time_to_frames(onset, sr=sr)
-    #     # print(frame)
-    #     pitch_values = pitches[:, onset]
-    #     magnitude_values = magnitudes[:, onset]
-    #     max_magnitude_index = np.argmax(magnitude_values)
-    #     extracted_pitches.append(pitch_values[max_magnitude_index])
-
-    # for t in range(pitches.shape[1]):
-    #     index = magnitudes[:, t].argmax()
-    #     pitch = pitches[index, t]
-    #     if pitch > 0:
-    #         audio_pitches.append(pitch)
-    #     else:
-    #         audio_pitches.append(0)
-
-
-    from scipy.spatial.distance import cdist
-    from librosa.sequence import dtw
-
-    midi_features = np.column_stack((midi_onsets, midi_pitches))
-    audio_features = np.column_stack((audio_onsets, extracted_pitches))
-
-    distance_matrix = cdist(midi_features, audio_features, metric='euclidean')
-    D, wp = dtw(C=distance_matrix, subseq=True)
-
-    # Extract aligned sequences
-    aligned_midi_onsets = midi_onsets[wp[:, 0]]
-    aligned_midi_pitches = midi_pitches[wp[:, 0]]
-    aligned_audio_onsets = audio_onsets[wp[:, 1]]
-    aligned_audio_pitches = extracted_pitches[wp[:, 1]]
-
-
-    # norm_spectrogram = norm_sg(spectrogram)
-
-    # midi_pitches = norm_midi_pitch(midi_pitches)
-
-    # norm_spectrogram = norm_spectrogram.flatten()
-
-    # midi_sequence = ["[MID]"] + [str(pitch) for pitch in midi_pitches]
-
-    # audio_sequence = ["[AUD]"] + [str(bin_value) for bin_value in norm_spectrogram]
-    
-    pass
-
-def main():
-    debug_vis = False
-    audio, sr = load_wav("C:\\personal_ML\\music-transcription\\maestro-v3.0.0\\maestro-v3.0.0\\2004\\MIDI-Unprocessed_SMF_02_R1_2004_01-05_ORIG_MID--AUDIO_02_R1_2004_05_Track05_wav.wav")
-    
-
-    midi = MidiFile('C:\\personal_ML\\music-transcription\\maestro-v3.0.0\\maestro-v3.0.0\\2004\\MIDI-Unprocessed_SMF_02_R1_2004_01-05_ORIG_MID--AUDIO_02_R1_2004_05_Track05_wav.midi', clip=True)
-    # print(mid)
-    
-    # audio = convert_to_mono(stereo_audio=audio)
-
-    
-    
-    # if sr != 16000:
-    #     audio = resample_audio(audio=audio, target_rate=16000, og_rate=sr)
+    midi = pretty_midi.PrettyMIDI(midi_path)
+    piano_roll = midi.get_piano_roll(fs=int(1/0.01))
 
     if debug_vis:
         plot_audio(audio=audio)
 
-    audio, midi = align(audio=audio, midi=midi, sr=sr)
-
-    # Convert audio to mel spectrogram
-    mel_spectrogram = audio_to_mel(audio, 16000)
-    mel_spectrogram_db = convert_to_DB(mel_spectrogram)
+    mel_spectrogram = audio_to_mel(audio=audio, sr=16000, n_mels=40)
+    mel_spectrogram_db = convert_to_DB(
+        mel_spectrogram=mel_spectrogram,
+        multiplier=10, 
+        amin=1e-10,
+        db_multiplier=0.0,
+        top_db=80.0
+        )
+    norm_db = norm_sg(mel_spectrogram_db)
+    
     if debug_vis:
         plot_spectrogram(mel_spectrogram_db.squeeze(0))
         plt.show()
 
-    
 
+def convert_dataset(audio_dir: Path, midi_dir: Path) -> None:
+    """
+    converts dataset of .wav and .midi files to tensors
+    """
+    for audio_path in audio_dir.iterdir():
+        matching_midi_path = midi_dir.glob(f"*{audio_path.name}.midi")[0]
+        create_tensor(
+            debug_vis=False,
+            audio_path=audio_path,
+            midi_path=matching_midi_path
+            )
+
+
+def main():
+    args = parse_cla()
+    convert_dataset(audio_dir=args.audio_dir, midi_dir=args.midi_dir)
 
 
 if __name__ == "__main__":
