e56e4039c39709e178d73be0808317b455a3900b
Daniel Franklin <118086192+d-f@users.noreply.github.com>
2024-10-01 01:35:56 -0600

    combined all models into one

--- train_gpt2.py
+++ train_gpt2.py
@@ -5,33 +5,45 @@ import torch.nn as nn
 from typing import Type
 
 
-def define_model(spec_len: int, pr_dim: int) -> Type[GPT2LMHeadModel]:
+class PianoGPT(nn.Module):
+    def __init__(self, input_dim, pr_dim, gpt, embed_dim, sg_dim, device):
+        super(PianoGPT, self).__init__()
+        self.input_dim = input_dim
+        self.gpt = gpt
+        self.sg_embed = nn.Linear(sg_dim, embed_dim)
+        self.pr_embed = nn.Linear(pr_dim, embed_dim)
+        self.embed_dim = embed_dim
+        self.device = device
+
+    def forward(self, x):
+        out = self.gpt(inputs_embeds=x)
+        next_token = out.logits[:, -1, :]
+        return next_token
+
+    def embed_pr(self, x):
+        pr_embed = self.pr_embed(x.transpose(1, 2))
+        eos_token = torch.zeros(size=(1, self.embed_dim)).unsqueeze(0).to(self.device)
+        pr_embed = torch.cat(tensors=[pr_embed, eos_token], dim=1)
+        return pr_embed
+    
+    def embed_sg(self, x):
+        embedded_input = self.sg_embed(x.transpose(1, 2))
+        sos_token = torch.zeros(size=(1, self.embed_dim)).unsqueeze(0).to(self.device)
+        embedded_input = torch.cat(tensors=[embedded_input, sos_token], dim=1)
+        return embedded_input
+
+
+def define_model(spec_len: int, pr_dim: int, embed_dim, sg_dim, device) -> Type[GPT2LMHeadModel]:
     """
     returns GPT2 model
     """
     config = GPT2Config.from_pretrained('gpt2')
     config.n_positions = spec_len
-    model = GPT2LMHeadModel(config)
-    model.resize_token_embeddings(new_num_tokens=pr_dim)
-    return model
-
-
-def define_sg_embedding(sg_dim, embed_dim):
-    """
-    returns the embedding model to transform
-    the spectrogram vector into the correct shape
-    for gpt2 input
-    """
-    return nn.Linear(sg_dim, embed_dim)
-
-
-def define_pr_embedding(pr_dim, embed_dim):
-    """
-    returns the embedding model to transform
-    the spectrogram vector into the correct shape
-    for gpt2 input
-    """
-    return nn.Linear(pr_dim, embed_dim)
+    gpt_model = GPT2LMHeadModel(config)
+    gpt_model.resize_token_embeddings(new_num_tokens=pr_dim)
+    piano_gpt = PianoGPT(input_dim=spec_len, pr_dim=pr_dim, gpt=gpt_model, embed_dim=embed_dim, sg_dim=sg_dim, device=device)
+    
+    return piano_gpt
 
 
 def train(num_epochs):
@@ -43,39 +55,30 @@ def train(num_epochs):
     sg_tensor = torch.tensor(sg_arr).to(device).unsqueeze(0)
     pr_tensor = torch.tensor(pr_arr).to(device, dtype=torch.float32).unsqueeze(0)
 
-    model = define_model(spec_len=5001, pr_dim=128).to(device)
-    sg_embedding = define_sg_embedding(sg_dim=40, embed_dim=model.config.n_embd).to(device)
-    pr_embedding = define_pr_embedding(pr_dim=128, embed_dim=model.config.n_embd).to(device)
-    optimizer = AdamW(list(pr_embedding.parameters()) + list(sg_embedding.parameters()) + list(model.parameters()), lr=1e-3)
+    model = define_model(spec_len=5001, pr_dim=128, embed_dim=768, sg_dim=40, device=device).to(device)
+    optimizer = AdamW(model.parameters(), lr=1e-3)
     criterion = torch.nn.CrossEntropyLoss()
 
-    embedded_input = sg_embedding(sg_tensor.transpose(1, 2))
-    sos_token = torch.zeros(size=(1, model.config.n_embd)).to(device).unsqueeze(0)
-
-    embedded_input = torch.cat(tensors=[embedded_input, sos_token], dim=1)
-
-    # transpose shape to (128, n) -> (n, 128) so that the vector can be
-    # interpreted as a sequence of 128-dim piano roll vectors rather than
-    # a separate sequence for each piano roll note
-    pr_embed = pr_embedding(pr_tensor.transpose(1, 2))
-    eos_token = torch.zeros(size=(1, model.config.n_embd)).to(device).unsqueeze(0)
-
-    pr_embed = torch.cat(tensors=[pr_embed, eos_token], dim=1)
-
     for epoch_idx in range(num_epochs):
-        for pr_idx in range(pr_embed.shape[1]):
-            output = model(inputs_embeds=embedded_input)
-            next_token = output.logits[:, -1, :]
-            loss = criterion(next_token, pr_tensor[:, :, pr_idx])
+        piano = model.embed_pr(pr_tensor)
+        embedded_input = model.embed_sg(sg_tensor)
+        pr_tensor_slices = torch.split(piano, split_size_or_sections=1, dim=2)
+
+        for pr_idx in range(len(pr_tensor_slices)):
+            next_token = model(embedded_input)
+            loss = criterion(next_token, pr_tensor_slices[pr_idx])
             print(loss)
             optimizer.zero_grad()
-            loss.backward()
+            if pr_idx < pr_tensor.shape[2] - 2:
+                loss.backward(retain_graph=True)  # Retain graph for future backpropagation
+            else:
+                loss.backward() 
             optimizer.step()
 
             # shift input and concatenate ground truth piano roll 
             # for teacher forcing
-            embedded_input = torch.cat(tensors=[embedded_input[:, 1:, :], pr_embed[:, pr_idx, :].unsqueeze(0)], dim=1)
-
+            embedded_input = torch.cat(tensors=[embedded_input[:, 1:, :], pr_tensor_slices[pr_idx].unsqueeze(0)], dim=1)
+        
 
 def main():
     train(num_epochs=10)
