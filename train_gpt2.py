b89775e248735354f55ea1ff1e86916a0750eba1
Daniel Franklin <118086192+d-f@users.noreply.github.com>
2024-10-02 20:37:27 -0600

    add quantization

--- train_gpt2.py
+++ train_gpt2.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 import torch
 import numpy as np
-from transformers import GPT2LMHeadModel, GPT2Config, AdamW
+from transformers import GPT2LMHeadModel, GPT2Config
 import torch.nn as nn
 from typing import Type
-from tqdm import tqdm
-from transformers import DataCollatorForLanguageModeling
-from transformers import GPT2LMHeadModel, GPT2Config, AdamW, Trainer, TrainingArguments
+from transformers import GPT2LMHeadModel, GPT2Config, Trainer, TrainingArguments
+import bitsandbytes as bnb
+from bitsandbytes.optim import Adam8bit
 
 
 class PianoGPT(nn.Module):
@@ -30,8 +30,8 @@ class PianoGPT(nn.Module):
 
     def forward(self, x, labels=None):
         sg_embedded = self.embed_sg(x)  
-      
-        sos_token = torch.zeros((x.shape[0], 1, self.embed_dim)).to(self.device) 
+
+        sos_token = torch.zeros((x.shape[0], 1, self.embed_dim)).to(self.device)  
         input_tokens = torch.cat([sos_token, sg_embedded], dim=1)  
         
         outputs = []
@@ -44,18 +44,15 @@ class PianoGPT(nn.Module):
             
             next_token_logits = gpt_output[:, -1, :]  
             
-            outputs.append(next_token_logits.unsqueeze(1)) 
-            
-           
-            next_token_embed = self.embed_pr(next_token_logits.float())  
+            outputs.append(next_token_logits.unsqueeze(1))  
+        
+            next_token_embed = self.embed_pr(next_token_logits.float()) 
             input_tokens = torch.cat([input_tokens[:, 1:, :], next_token_embed.unsqueeze(1)], dim=1) 
 
-           
             if labels is not None:
                 target_token = labels[:, :, :, t].squeeze(0).to(self.device) 
                 loss += self.loss_fn(next_token_logits, target_token.squeeze(1))  
             
-  
         outputs = torch.cat(outputs, dim=1)
         
         return (loss, outputs) if labels is not None else outputs
@@ -102,22 +99,24 @@ def define_model(spec_len: int, pr_dim: int, embed_dim, sg_dim, device) -> Type[
     returns GPT2 model
     """
     config = GPT2Config.from_pretrained('gpt2')
-    config.n_positions = spec_len
-    config.n_inner = int(768 / 2)
-    config.n_layer = 2
-    config.n_heads = 2
+    config.n_inner = int(768 / 5)
+    config.n_layer = 1
+    config.n_heads = 1
+    config.n_positions = 512
     gpt_model = GPT2LMHeadModel(config)
     gpt_model.resize_token_embeddings(new_num_tokens=pr_dim)
     gpt_model.gradient_checkpointing_enable()
+    for param in gpt_model.parameters():
+        param.data = bnb.nn.Int8Params(param.data, requires_grad=True)
     piano_gpt = PianoGPT(input_dim=spec_len, pr_dim=pr_dim, gpt=gpt_model, embed_dim=embed_dim, sg_dim=sg_dim, device=device)
     
     return piano_gpt
 
 
 def collate_fn(batch):
-    max_sg_len = max([item['x'].shape[2] for item in batch]) 
-    max_pr_len = max([item['label_ids'].shape[2] for item in batch]) 
-    
+    max_sg_len = max([item['x'].shape[2] for item in batch])  
+    max_pr_len = max([item['label_ids'].shape[2] for item in batch])  
+
     sg_padded = []
     pr_padded = []
     
@@ -125,14 +124,11 @@ def collate_fn(batch):
         sg = item['x']
         pr = item['label_ids']
         
-        
         pad_len_sg = max_sg_len - sg.shape[2]
         sg_padded.append(torch.nn.functional.pad(sg, (0, pad_len_sg)))
         
-        
         pad_len_pr = max_pr_len - pr.shape[2]
         pr_padded.append(torch.nn.functional.pad(pr, (0, pad_len_pr)))
-    
     
     sg_batch = torch.stack(sg_padded)
     pr_batch = torch.stack(pr_padded)
@@ -148,11 +144,10 @@ def train(num_epochs):
     for param in model.parameters():
         param.requires_grad = True
 
-   
     training_args = TrainingArguments(
         output_dir="./results",
-        per_device_train_batch_size=2,
-        num_train_epochs=10,
+        per_device_train_batch_size=70,
+        num_train_epochs=num_epochs,
         logging_dir="C:\\personal_ML\\music-transcription\\logs",
         learning_rate=1e-3,
         logging_steps=1,
@@ -162,7 +157,8 @@ def train(num_epochs):
         model=model,
         args=training_args,
         train_dataset=dataset,
-        data_collator=collate_fn
+        data_collator=collate_fn,
+        optimizers=(Adam8bit(model.parameters(), lr=1e-3), None)
     )
 
     trainer.train()
