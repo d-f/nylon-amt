947ff7cf304ffe571e07d02fc4c7ee34542f1bf9
Daniel Franklin <118086192+d-f@users.noreply.github.com>
2024-10-03 15:22:04 -0600

    edit architecture and change device management in forward pass
    make slight changes to the architecture and removes adding the models to the device within the forward pass

--- train_gpt2.py
+++ train_gpt2.py
@@ -10,23 +10,28 @@ from bitsandbytes.optim import Adam8bit
 
 
 class PianoGPT(nn.Module):
-    def __init__(self, input_dim, pr_dim, gpt, embed_dim, sg_dim, device):
+    def __init__(self, input_dim, pr_dim, gpt, embed_dim, sg_dim, device, temp):
         super(PianoGPT, self).__init__()
         self.input_dim = input_dim
         self.gpt = gpt
         self.sg_embed = nn.Sequential(
                             nn.Linear(sg_dim, embed_dim),
                             nn.LayerNorm(embed_dim), 
-                            nn.Dropout(0.1)
+                            nn.Dropout(0.2)
                         )
         self.pr_embed = nn.Sequential(
                             nn.Linear(pr_dim, embed_dim),
                             nn.LayerNorm(embed_dim),  
-                            nn.Dropout(0.1)
+                            nn.Dropout(0.2)
                         )
         self.embed_dim = embed_dim
         self.device = device
         self.loss_fn = nn.CrossEntropyLoss()
+
+        self.gpt.to(device)
+        self.pr_embed.to(device)
+        self.temperature = temp
+
 
     def forward(self, x, labels=None):
         sg_embedded = self.embed_sg(x)  
@@ -36,12 +41,9 @@ class PianoGPT(nn.Module):
         
         outputs = []
         loss = 0
-        self.gpt.to(self.device)
-        self.pr_embed.to(self.device)
 
         for t in range(1, sg_embedded.size(1) + 1):  
             gpt_output = self.gpt(inputs_embeds=input_tokens).logits 
-            
             next_token_logits = gpt_output[:, -1, :]  
             
             outputs.append(next_token_logits.unsqueeze(1))  
@@ -50,8 +52,8 @@ class PianoGPT(nn.Module):
             input_tokens = torch.cat([input_tokens[:, 1:, :], next_token_embed.unsqueeze(1)], dim=1) 
 
             if labels is not None:
-                target_token = labels[:, :, :, t].squeeze(0).to(self.device) 
-                loss += self.loss_fn(next_token_logits, target_token.squeeze(1))  
+                target_token = labels.squeeze(1)[:, :, t]
+                loss += self.loss_fn(next_token_logits, target_token)  
             
         outputs = torch.cat(outputs, dim=1)
         
@@ -87,28 +89,30 @@ class PianoDataset(torch.utils.data.Dataset):
         sg_arr = np.load(sg_path)
         pr_arr = np.load(pr_path)
         
-        sg_tensor = torch.tensor(sg_arr, requires_grad=True).unsqueeze(0)
+        sg_tensor = torch.tensor(sg_arr, requires_grad=True).to(dtype=torch.float32).unsqueeze(0)
         pr_tensor = torch.tensor(pr_arr, requires_grad=True).to(dtype=torch.float32).unsqueeze(0)
         pr_tensor /= self.pr_max
         
         return {"x": sg_tensor, "label_ids": pr_tensor}
 
 
-def define_model(spec_len: int, pr_dim: int, embed_dim, sg_dim, device) -> Type[GPT2LMHeadModel]:
+def define_model(spec_len: int, pr_dim: int, embed_dim, sg_dim, device, temp) -> Type[GPT2LMHeadModel]:
     """
     returns GPT2 model
     """
+
     config = GPT2Config.from_pretrained('gpt2')
-    config.n_inner = int(768 / 5)
-    config.n_layer = 1
-    config.n_heads = 1
+    config.n_inner = 256 
+    config.n_layer = 2
+    config.n_heads = 2
     config.n_positions = 512
+    config.n_embd = embed_dim
     gpt_model = GPT2LMHeadModel(config)
     gpt_model.resize_token_embeddings(new_num_tokens=pr_dim)
     gpt_model.gradient_checkpointing_enable()
     for param in gpt_model.parameters():
         param.data = bnb.nn.Int8Params(param.data, requires_grad=True)
-    piano_gpt = PianoGPT(input_dim=spec_len, pr_dim=pr_dim, gpt=gpt_model, embed_dim=embed_dim, sg_dim=sg_dim, device=device)
+    piano_gpt = PianoGPT(input_dim=spec_len, pr_dim=pr_dim, gpt=gpt_model, embed_dim=embed_dim, sg_dim=sg_dim, device=device, temp=temp)
     
     return piano_gpt
 
@@ -136,20 +140,25 @@ def collate_fn(batch):
     return {"x": sg_batch, "labels": pr_batch}
 
 
+def replace_with_8bit_linear(model):
+    for name, module in model.named_children():
+        if isinstance(module, nn.Linear):
+            setattr(model, name, bnb.nn.Linear8bitLt(module.in_features, module.out_features, has_fp16_weights=False))
+
+
 def train(num_epochs):
     torch.autograd.set_detect_anomaly(True)
     device = torch.device("cuda")
     dataset = PianoDataset(device=device, pr_max=204, file_dir=Path("C:\\personal_ML\\music-transcription\\save\\"))
-    model = define_model(spec_len=100+1, pr_dim=128, embed_dim=768, sg_dim=40, device=device).to(device)
-    for param in model.parameters():
-        param.requires_grad = True
+    model = define_model(spec_len=100+1, pr_dim=128, embed_dim=144, sg_dim=40, device=device, temp=0.7).to(device)
+    replace_with_8bit_linear(model)
+    lr = 1e-5
 
     training_args = TrainingArguments(
         output_dir="./results",
-        per_device_train_batch_size=70,
+        per_device_train_batch_size=200,
         num_train_epochs=num_epochs,
         logging_dir="C:\\personal_ML\\music-transcription\\logs",
-        learning_rate=1e-3,
         logging_steps=1,
         max_grad_norm=1.0,
     )
@@ -158,7 +167,7 @@ def train(num_epochs):
         args=training_args,
         train_dataset=dataset,
         data_collator=collate_fn,
-        optimizers=(Adam8bit(model.parameters(), lr=1e-3), None)
+        optimizers=(Adam8bit(model.parameters(), lr=lr), None)
     )
 
     trainer.train()
