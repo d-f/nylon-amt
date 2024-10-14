86aa0b6e07363351f72febaaf037dde712be6a7a
Daniel Franklin <118086192+d-f@users.noreply.github.com>
2024-10-13 23:16:24 -0600

    use shuffling instead of sampling and limit the amount of list comprehension

--- dataset/partition_dataset.py
+++ dataset/partition_dataset.py
@@ -1,54 +1,55 @@
 import csv
 import random
 from pathlib import Path
+from typing import List, Tuple
 
 
 def convert_path(sg_filepath: Path) -> Path:
     """
-    converts the name of an spectrogram filename to the
+    converts the name of a spectrogram filename to the
     corresponding piano roll filename
     """
-    pr_filename = sg_filepath.name.rpartition("-spec.npy")[0] + "-piano.npy"
-    pr_path = sg_filepath.parent.joinpath(pr_filename)
-    return pr_path
+    return sg_filepath.with_name(sg_filepath.stem.replace("-spec", "-piano") + ".npy")
 
 
-def partition_datasets(sg_files, val_prop):
-    val_amt = int(len(sg_files) * val_prop)
-    val_sg = random.sample(sg_files, val_amt)
-
-    sg_files = [x for x in sg_files if x not in val_sg]
-
-    test_sg = random.sample(sg_files, val_amt)
-    sg_files = [x for x in sg_files if x not in test_sg]
-
-    train_sg = sg_files
-
-    val_pr = [convert_path(x) for x in val_sg]
-    test_pr = [convert_path(x) for x in test_sg]
-    train_pr = [convert_path(x) for x in train_sg]
-
-    return val_sg, val_pr, test_sg, test_pr, train_sg, train_pr
+def partition_datasets(sg_files: List[Path], val_prop: float) -> Tuple[List[Path], List[Path], List[Path]]:
+    """
+    partitions the files into train, validation and test datasets
+    """
+    random.shuffle(sg_files)
+    total = len(sg_files)
+    val_amt = int(total * val_prop)
+    
+    val_sg = sg_files[:val_amt]
+    test_sg = sg_files[val_amt:2*val_amt]
+    train_sg = sg_files[2*val_amt:]
+    
+    return val_sg, test_sg, train_sg
 
 
-def save_csv(csv_path, csv_list):
+def save_csv(csv_path: Path, csv_list: List[Path]):
+    """
+    saves a csv file
+    """
     with open(csv_path, mode="w", newline="") as opened_csv:
         writer = csv.writer(opened_csv)
-        for row in csv_list:
-            writer.writerow([row,])
+        writer.writerows([[str(row)] for row in csv_list])
 
 
 def main():
-    file_dir=Path("C:\\personal_ML\\music-transcription\\save\\")
-    sg_files = [x for x in file_dir.iterdir() if "spec" in str(x)]
-    val_sg, val_pr, test_sg, test_pr, train_sg, train_pr = partition_datasets(sg_files=sg_files, val_prop=0.1)
-    save_csv(csv_list=val_sg, csv_path=Path("C:\\personal_ML\\nylon_gpt\\dataset_csv\\val_sg.csv"))
-    save_csv(csv_list=val_pr, csv_path=Path("C:\\personal_ML\\nylon_gpt\\dataset_csv\\val_pr.csv"))
-    save_csv(csv_list=test_sg, csv_path=Path("C:\\personal_ML\\nylon_gpt\\dataset_csv\\test_sg.csv"))
-    save_csv(csv_list=test_pr, csv_path=Path("C:\\personal_ML\\nylon_gpt\\dataset_csv\\test_pr.csv"))
-    save_csv(csv_list=train_sg, csv_path=Path("C:\\personal_ML\\nylon_gpt\\dataset_csv\\train_sg.csv"))
-    save_csv(csv_list=train_pr, csv_path=Path("C:\\personal_ML\\nylon_gpt\\dataset_csv\\train_pr.csv"))
-
+    file_dir = Path("C:\\personal_ML\\nylon_gpt\\processed_data\\")
+    sg_files = [x for x in file_dir.iterdir() if "spec" in x.name]
+    
+    val_sg, test_sg, train_sg = partition_datasets(sg_files, val_prop=0.1)
+    
+    dataset_types = ['val', 'test', 'train']
+    sg_datasets = [val_sg, test_sg, train_sg]
+    
+    for dtype, sg_dataset in zip(dataset_types, sg_datasets):
+        pr_dataset = [convert_path(x) for x in sg_dataset]
+        
+        save_csv(Path(f"C:\\personal_ML\\nylon_gpt\\dataset_csv\\{dtype}_sg.csv"), sg_dataset)
+        save_csv(Path(f"C:\\personal_ML\\nylon_gpt\\dataset_csv\\{dtype}_pr.csv"), pr_dataset)
 
 
 if __name__ == "__main__":
