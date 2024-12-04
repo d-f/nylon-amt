3278005ac61aa32b2bd0003c28783053da56cdc5
Daniel Franklin <118086192+d-f@users.noreply.github.com>
2024-12-04 08:02:40 -0700

    add model summary and explicitly enable gradient updates for all parameters

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ from transformers import GPT2LMHeadModel, GPT2Config
 from typing import Type
 from transformers import GPT2LMHeadModel, GPT2Config, Trainer, TrainingArguments
 from model.nylon_gpt import NylonGPT
-from model.model_utils import EarlyStoppingCallback
+from model.model_utils import EarlyStoppingCallback, model_summary, enable_all_parameters
 from dataset.data_utils import PianoRollDataset, collate_fn, open_csv
 
 
@@ -123,18 +123,18 @@ def parse_cla() -> Type[argparse.Namespace]:
     parser.add_argument("-sg_dim", type=int, default=40) # number of spectrogram features
     parser.add_argument("-embed_dim", type=int, default=200) # size of the embedding dimension
     parser.add_argument("-lr", type=float, default=1e-4) # learning rate
-    parser.add_argument("-bs", type=int, default=100) # batch size
+    parser.add_argument("-bs", type=int, default=200) # batch size
     parser.add_argument("-patience", type=int, default=5) # number of times to allow for stopping criteria to be met consecutively
     parser.add_argument("-patience_thresh", type=float, default=0.01) # threshold to consider loss having improved or not
     parser.add_argument("-output_dir", type=str, default="C:\\personal_ML\\nylon_gpt\\training_results\\model_3") # folder to save model checkpoint to
-    parser.add_argument("-num_epochs", type=int, default=1) # number of training iterations
-    parser.add_argument("-n_inner", type=int, default=512) # dimensionality of feed forward layers in transformer
-    parser.add_argument("-n_layer", type=int, default=4) # number of hidden layers in the transformer
-    parser.add_argument("-n_head", type=int, default=4) # number of attention heads
+    parser.add_argument("-num_epochs", type=int, default=10) # number of training iterations
+    parser.add_argument("-n_inner", type=int, default=256) # dimensionality of feed forward layers in transformer
+    parser.add_argument("-n_layer", type=int, default=1) # number of hidden layers in the transformer
+    parser.add_argument("-n_head", type=int, default=1) # number of attention heads
     parser.add_argument("-n_positions", type=int, default=150) # maximum generated sequence length
     parser.add_argument("-chkpt_num", type=int, default=None) # number on the checkpoint folder name e.g. checkpoint-100
     parser.add_argument("-csv_dir", type=Path, default=Path("C:\\personal_ML\\nylon_gpt\\dataset_csv\\")) # folder that contains dataset csvs
-    parser.add_argument("-ds_prop", type=float, default=0.50) # proportion of the dataset to use
+    parser.add_argument("-ds_prop", type=float, default=0.01) # proportion of the dataset to use
     
     return parser.parse_args()
 
@@ -184,7 +184,10 @@ def main():
         n_head=args.n_head,
         n_positions=args.n_positions
         )
-
+    
+    enable_all_parameters(model)
+    model_summary(model)
+    
     if args.chkpt_num:
         train(
             num_epochs=args.num_epochs,
